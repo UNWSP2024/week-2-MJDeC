13202eb24cbe82d74adb807b1895ddec5bba9727
MJDeC <mjdecaro@students.unwsp.edu>
2024-09-12 09:45:38 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,9 +1,13 @@
+#Micah DeCaro, 9/12/24, Personal Information
+#Program which prints my own personal information.
 def personal_information():
-    print("""Micah DeCaro
+    print("""
+    Micah DeCaro
     618 Rice St. W 
     Stillwater, MN 55082
     651-430-0544
-    Music Education""")
+    Music Education
+    """)
 
 
 # Line which calls the above function.
