88e1ea6329361a7026ed67cfdad43a67779172d4
MJDeC <mjdecaro@students.unwsp.edu>
2024-09-12 09:04:40 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,18 @@
 
 def average_age():
     # Get User Input
-
-
+age1=float(input('Enter your age:'))
+age2=float(input('Enter your age:'))
+age3=float(input('Enter your age:'))
+age4=float(input('Enter your age:'))
+age5=float(input('Enter your age:'))
+            
     # Sum ages
-
+average=(age1+age2+age3+age4+age5)
     # Average the ages
+final_average=average/5.0
 
     # Print the results
-
+print(final_average)
 # Line which calls the above function.
 average_age()
