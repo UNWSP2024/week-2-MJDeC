b1a1f4ae4463f72b60e7ca8dc61cd1e74443e06d
MJDeC <mjdecaro@students.unwsp.edu>
2024-09-13 09:57:08 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -5,16 +5,11 @@ def calculate_total_purchase():
     # then displays the subtotal of the sale, 
     # the amount of sales tax, and the total.  
     # Assume the sales tax is 7 percent.
-    print("What is the price of item 1 in dollars? ")
-    item1=float(input())
-    print("What is the price of item 2 in dollars? ")
-    item2=float(input())
-    print("What is the price of item 3 in dollars? ")
-    item3=float(input())
-    print("What is the price of item 4 in dollars? ")
-    item4=float(input())
-    print("What is the price of item 5 in dollars? ")
-    item5=float(input())
+    item1=float(input("What is the price of item 1 in dollars? "))
+    item2=float(input("What is the price of item 2 in dollars? "))
+    item3=float(input("What is the price of item 3 in dollars? "))
+    item4=float(input("What is the price of item 4 in dollars?"))
+    item5=float(input("What is the price of item 5 in dollars? "))
 
     subtotal=(item1+item2+item3+item4+item5)
     sales_tax=subtotal*.07
