6948641238059237004b083f12c32ce06cc3a476
MJDeC <mjdecaro@students.unwsp.edu>
2024-09-13 08:30:04 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -2,11 +2,16 @@
 #Program that asks for ages of five friends and computes the average.
 def average_age():
     # Get User Input
-age1=float(input('Enter your age, Friend One: '))
-age2=float(input('Enter your age, Friend Two: '))
-age3=float(input('Enter your age, Friend Three: '))
-age4=float(input('Enter your age, Friend Four: '))
-age5=float(input('Enter your age, Friend Five: '))
+print('Enter your age, Friend One: ')
+age1=float(input())
+print('Enter your age, Friend Two: ')
+age2=float(input())
+print('Enter your age, Friend Three: ')
+age3=float(input())
+print('Enter your age, Friend Four: ')
+age4=float(input())
+print('Enter your age, Friend Five: ')
+age5=float(input())
             
     # Sum ages
 sum=(age1+age2+age3+age4+age5)
